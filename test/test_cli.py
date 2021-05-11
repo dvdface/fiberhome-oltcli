69727a26ea87dff5fb1e92c12d363d61d7bd46d8
DingYi <dvdface@users.noreply.github.com>
2021-05-11 10:44:02 +0800

    Update test_cli.py

--- test/test_cli.py
+++ test/test_cli.py
@@ -2,5 +2,5 @@ from oltcli.cli import OLTModel, OLTCLI
 
 def test_oltcli():
 
-    oltcli = OLTCLI.get(OLTModel.AN6000_17, '10.182.33.210', 'GEPON', 'GEPON')
+    oltcli = OLTCLI.get(OLTModel.AN6000_17, '10.182.33.210', 'GPON', 'GPON')
     assert type(oltcli.get_authorization()) == list
